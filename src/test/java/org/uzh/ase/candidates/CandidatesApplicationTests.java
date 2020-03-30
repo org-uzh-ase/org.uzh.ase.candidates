360d7ab92baf0f9019e06c21b81c8ebfb6404abf
ltoedt <laura.toedtli@uzh.ch>
2020-03-30 08:30:47 +0200

    initial commit

---
+++ src/test/java/org/uzh/ase/candidates/CandidatesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uzh.ase.candidates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
