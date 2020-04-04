34a979b103382a2f9ada4fc463f5790b344cb360
ltoedt <laura.toedtli@uzh.ch>
2020-04-04 17:33:42 +0200

    removed unused import

--- src/main/java/org/uzh/ase/candidates/model/Candidates.java
+++ src/main/java/org/uzh/ase/candidates/model/Candidates.java
@@ -1,7 +1,6 @@
 package org.uzh.ase.candidates.model;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("candidates")
