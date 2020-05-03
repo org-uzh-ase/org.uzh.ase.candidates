185b112271a848f16475c4153d42d1678b77fc42
ltoedt <laura.toedtli@uzh.ch>
2020-05-03 14:19:08 +0200

    added title to candidate

--- src/test/java/org/uzh/ase/candidates/model/CandidateTest.java
+++ src/test/java/org/uzh/ase/candidates/model/CandidateTest.java
@@ -12,6 +12,7 @@ public class CandidateTest {
     private final String defaultID = "42";
     private final String defaultPosterURL = "https://images-na.ssl-images-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg";
     private final String defaultGenre = "Documentary";
+    private final String defaultTitle = "Title of the Movie";
 
     @Test
     public void testNoArgConstructor(){
@@ -27,11 +28,13 @@ public class CandidateTest {
         String id = "5678";
         String posterUrl = "https://reddit.co./some.jpg";
         String genre = "Apple juice";
+        String title = "Tralala";
 
-        Candidate candidate = new Candidate(id, posterUrl, genre);
+        Candidate candidate = new Candidate(id, posterUrl, genre, title);
         assertEquals(id, candidate.getId(), "Expected value for ID used in constructor is different.");
         assertEquals(posterUrl, candidate.getPoster(), "Expected value for Poster URL used in constructor is different.");
         assertEquals(genre, candidate.getGenre(), "Expected value for Genre used in constructor is different.");
+        assertEquals(title, candidate.getTitle(), "Expected value for Title used in constructor is different.");
     }
 
     @Test
@@ -40,34 +43,37 @@ public class CandidateTest {
 
         assertFalse(c.equals(null));
         assertFalse(c.equals(new Object()));
-        assertFalse(c.equals(new Candidate("Different ID", "Different poster URL", "Different genre")));
+        assertFalse(c.equals(new Candidate("Different ID", "Different poster URL", "Different genre", "Different title")));
 
-        assertFalse(c.equals(new Candidate("Different ID", "Different poster URL", defaultGenre)));
-        assertFalse(c.equals(new Candidate("Different ID", defaultPosterURL, "Different genre")));
-        assertFalse(c.equals(new Candidate(defaultID, "Different poster URL", "Different genre")));
+        assertFalse(c.equals(new Candidate("Different ID", "Different poster URL", defaultGenre,"Different title")));
+        assertFalse(c.equals(new Candidate("Different ID", defaultPosterURL, "Different genre", "Different title")));
+        assertFalse(c.equals(new Candidate(defaultID, "Different poster URL", "Different genre", "Different title")));
+        assertFalse(c.equals(new Candidate("Different ID", "Different poster URL", "Different genre", defaultTitle)));
 
-        assertFalse(c.equals(new Candidate("Different ID", defaultPosterURL, defaultGenre)));
-        assertFalse(c.equals(new Candidate(defaultID, "Different poster URL", defaultGenre)));
-        assertFalse(c.equals(new Candidate(defaultID, defaultPosterURL, "Different genre")));
+        assertFalse(c.equals(new Candidate("Different ID", defaultPosterURL, defaultGenre, "Different title")));
+        assertFalse(c.equals(new Candidate(defaultID, "Different poster URL", defaultGenre,"Different title")));
+        assertFalse(c.equals(new Candidate(defaultID, defaultPosterURL, "Different genre", "Different title")));
+        assertFalse(c.equals(new Candidate("Different ID", "Different poster URL", defaultGenre, defaultTitle)));
 
         assertTrue(c.equals(c));
-        assertTrue(c.equals(new Candidate(defaultID, defaultPosterURL, defaultGenre)));
+        assertTrue(c.equals(new Candidate(defaultID, defaultPosterURL, defaultGenre, defaultTitle)));
     }
 
     @Test
     public void testHashCode() {
         Candidate c = new Candidate();
         assertEquals(c.hashCode(), new Candidate().hashCode());
-        assertEquals(c.hashCode(), new Candidate(defaultID, defaultPosterURL, defaultGenre).hashCode());
+        assertEquals(c.hashCode(), new Candidate(defaultID, defaultPosterURL, defaultGenre, defaultTitle).hashCode());
 
-        assertNotEquals(c.hashCode(), new Candidate("Different ID", "Different poster URL", "Different genre").hashCode());
+        assertNotEquals(c.hashCode(), new Candidate("Different ID", "Different poster URL", "Different genre", "Different title").hashCode());
 
-        assertNotEquals(c.hashCode(), new Candidate("Different ID", "Different poster URL", defaultGenre).hashCode());
-        assertNotEquals(c.hashCode(), new Candidate("Different ID", defaultPosterURL, "Different genre").hashCode());
-        assertNotEquals(c.hashCode(), new Candidate(defaultID, "Different poster URL", "Different genre").hashCode());
+        assertNotEquals(c.hashCode(), new Candidate("Different ID", "Different poster URL", defaultGenre, "Different title").hashCode());
+        assertNotEquals(c.hashCode(), new Candidate("Different ID", defaultPosterURL, "Different genre", "Different title").hashCode());
+        assertNotEquals(c.hashCode(), new Candidate(defaultID, "Different poster URL", "Different genre", "Different title").hashCode());
 
-        assertNotEquals(c.hashCode(), new Candidate("Different ID", defaultPosterURL, defaultGenre).hashCode());
-        assertNotEquals(c.hashCode(), new Candidate(defaultID, "Different poster URL", defaultGenre).hashCode());
-        assertNotEquals(c.hashCode(), new Candidate(defaultID, defaultPosterURL, "Different genre").hashCode());
+        assertNotEquals(c.hashCode(), new Candidate("Different ID", defaultPosterURL, defaultGenre, "Different title").hashCode());
+        assertNotEquals(c.hashCode(), new Candidate(defaultID, "Different poster URL", defaultGenre, "Different title").hashCode());
+        assertNotEquals(c.hashCode(), new Candidate(defaultID, defaultPosterURL, "Different genre", "Different title").hashCode());
+        assertNotEquals(c.hashCode(), new Candidate("Different ID", "Different poster URL", defaultGenre, defaultTitle).hashCode());
     }
 }
