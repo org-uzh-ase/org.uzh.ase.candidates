1e1bf240fd94be2a31b2a0e69df1c5977a0bb346
Vladimir Masarik <masarik.vladimir7@gmail.com>
2020-05-09 15:09:23 +0200

    added environment specific config

--- src/main/java/org/uzh/ase/candidates/CandidatesApplication.java
+++ src/main/java/org/uzh/ase/candidates/CandidatesApplication.java
@@ -4,12 +4,14 @@ package org.uzh.ase.candidates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@PropertySource({ "classpath:application-${env:local}.properties" })
 public class CandidatesApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CandidatesApplication.class);
 	}
