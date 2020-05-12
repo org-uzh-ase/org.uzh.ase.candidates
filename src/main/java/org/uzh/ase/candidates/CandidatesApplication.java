b6f8aeb54b9c121e394377cd28d8384c95eeffca
Vladimir Masarik <masarik.vladimir7@gmail.com>
2020-05-12 12:17:27 +0200

    removed CORS example

--- src/main/java/org/uzh/ase/candidates/CandidatesApplication.java
+++ src/main/java/org/uzh/ase/candidates/CandidatesApplication.java
@@ -3,10 +3,7 @@ package org.uzh.ase.candidates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 /**
@@ -23,16 +20,5 @@ public class CandidatesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CandidatesApplication.class);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/example").allowedOrigins("http://localhost:8080");
-			}
-		};
-	}
-
 
 }
